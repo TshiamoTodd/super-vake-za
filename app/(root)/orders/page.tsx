d86fb44a6354dc766e58ace67d9ddf6afc84a9f6
TshiamoTodd <tshiamo.mokwena19@gmail.com>
2024-01-27 16:24:17 +0200

    fix search bug

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -17,7 +17,7 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="wrapper mt-8">
-        <Search placeholder="Search buyer name..." />
+        <Search placeHolder="Search buyer name..." />
       </section>
 
       <section className="wrapper overflow-x-auto">
